caa5dc9a35479d7e5c1a8b2e6af2ceb2043afebd
cale2107 <cale2107@yahoo.com>
2015-12-15 15:54:56 +0100

    vratio

--- test2/brendovi.php
+++ test2/brendovi.php
@@ -24,7 +24,7 @@
 				<a href="#" class="fb right">Facebook</a>
 			</div>
 		</div><!-- / Top -->
-		<a href="#" class="logo">Plusplus</a>
+		<a href="#" class="logo">PlusPlus</a>
 		<nav>
 			<ul>
 				<li class="home"><a href="index.php">POČETNA</a></li>
